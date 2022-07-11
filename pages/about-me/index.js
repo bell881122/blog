5f69b361e1ead4ed3cbcad46d755e1370ea3ee8a
emi <bell881122@gmail.com>
2022-07-11 22:42:13 +0800

    add about-me page

---
+++ pages/about-me/index.js
@@ -0,0 +1,20 @@
+export default function Resume() {
+  return (
+    <div className="m-6">
+      <div className="flex-none md:flex">
+        <div>
+          <h1 className="text-[40px]">張 郁欣</h1>
+          <h2 className="text-[20px] font-bold">Yu-hsin Chang</h2>
+          <p className="mt-4">Front-end developer</p>
+        </div>
+        <div className="md:ml-auto md:mt-auto">
+          <p>Taipei Taiwan.</p>
+          <p>bell881122@gmail.com</p>
+          <p>LinkedIn</p>
+          <p>Github</p>
+        </div>
+      </div>
+      {/* Section2 */}
+    </div>
+  )
+}
